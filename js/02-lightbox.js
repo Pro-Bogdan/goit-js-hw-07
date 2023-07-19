c9de214146d924072ea41390e3aabcc6904d3221
Valentyna Protasenko <b.protasenko@gmail.com>
2023-07-19 18:47:21 +0200

    second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEL = document.querySelector(".gallery");
+galleryEL.insertAdjacentHTML("afterBegin", createGalleryMarkUp(galleryItems));
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function createGalleryMarkUp(arrayImg) {
+  return arrayImg
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    )
+    .join("");
+}
